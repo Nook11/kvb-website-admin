245839474cdefe9685e72cae7835417e57222d4c
Nook11 <131190059+Nook11@users.noreply.github.com>
2025-08-05 16:18:14 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
 module.exports = function(eleventyConfig) {
-  // คำสั่งนี้สำคัญมาก: บอกให้ Eleventy ก็อปปี้โฟลเดอร์ imges, admin และไฟล์ style.css
-  // ไปยังเว็บไซต์เวอร์ชันสุดท้ายด้วย เพื่อให้ทุกอย่างยังแสดงผลเหมือนเดิม
+  
   eleventyConfig.addPassthroughCopy("imges");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("style.css");
 
-  // เพิ่มฟังก์ชันสำหรับดึงปีปัจจุบัน (ถ้าต้องการใช้ใน footer)
+
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
@@ -14,4 +13,5 @@ module.exports = function(eleventyConfig) {
       includes: "_includes",
       output: "_site",
     },
-  }
+  };
+}; 
